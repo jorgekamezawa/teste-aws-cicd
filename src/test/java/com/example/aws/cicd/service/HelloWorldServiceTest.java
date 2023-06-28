5596f509509c5d33238a84e5385171f28335b120
Jorge Kamezawa <jor_kamezawa@hotmail.com>
2023-06-28 20:05:47 -0300

    testing deploy

--- src/test/java/com/example/aws/cicd/service/HelloWorldServiceTest.java
+++ src/test/java/com/example/aws/cicd/service/HelloWorldServiceTest.java
@@ -17,7 +17,7 @@ class HelloWorldServiceTest {
     @Test
     void testGetHelloWorld() {
         // Given
-        HelloWorld expectedHelloWorld = new HelloWorld("Hello", "World");
+        HelloWorld expectedHelloWorld = new HelloWorld("Hello", "New World");
 
         // When
         HelloWorld actualHelloWorld = service.getHelloWorld();
